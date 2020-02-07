121cdab3d4be8bc5635b465a9d233316c85bfe60
angelabenavente <angela.b.almazan@gmail.com>
2020-02-07 19:30:15 +0100

    Create a var to thought types pokemon

--- main.js
+++ main.js
@@ -18,7 +18,17 @@ const app = new Vue ( {
         "experience":64,
         "height":7,
         "weight":69
-     }
+      },
+      colorType: {
+        "grass": "#78C850",
+        "poison": "#A040A0",
+        "fire": "#F08030",
+        "flying": "#A890F0",
+        "water": "#6890F0",
+        "bug": "#A8B820",
+        "normal": "#A8A878",
+        "electric": "#F8D030"
+      }
     }
   },
 })
